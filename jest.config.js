c0a8c50cdcffa4a23582b876fef78f405af2e495
Chinonso-Peter <chinonsopeter456@gmail.com>
2026-03-25 16:48:09 +0100

    Implemented  Access Control List (ACL) Contract

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
-};
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json']
 };
